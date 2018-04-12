680b9a740991a53a10eea1363465f394c7621475
ghooost <ghooost@gmail.com>
2018-04-12 10:36:14 +0300

    12.04 start

--- foxapi.php
+++ foxapi.php
@@ -1,12 +1,43 @@
 <?php
 class FoxApi {
-  function __construct($db){
-    $this->db=$db;
+  function __construct($debugMode=false, $dbConnect="",$apiURL=""){
+    $def=array(
+      "dbConnect"=>"Provider = VFPOLEDB.1; Data Source = \"C:\\Users\\Dev\\Desktop\\FoxProProj\\db_test\\a0a.dbf\"",
+      "apiURL"=>"http://192.168.3.54:5002/kz"
+    );
+
+    $this->connStr=$dbConnect!=""?$dbConnect:$def['dbConnect'];
+    $this->apiURL=$apiURL!=""?$apiURL:$def['apiURL'];
+
+    $this->debugMode=$debugMode;
   }
+
+  function connectDB($connStr){
+    $this->db = new COM("ADODB.Connection") or die("Cannot start ADO");
+    $this->db->Open($this->connStr);
+  }
+
+  function closeDB(){
+    $this->db->Close();
+  }
+
   function saveData($str){
     $data=json_decode($str,TRUE);
-    print_r($data);
+    foreach($data as $v){
+      $this->insertItem($v);
+    };
   }
+
+  function insertItem($data){
+    foreach($data['OtherInfo'] as $k=>$v) echo $k,'=>',$v,'<br>';
+    $itemData=$this->processRootSection($data,array());
+    //print_r($data);
+  }
+
+  function processRootSection($dataIn,$dataOut){
+    $dataOut['type_doc_id']
+  }
+
   function insert($table,$data){
     $fields=array_keys($data);
     $values=array_values($data);
@@ -16,11 +47,26 @@ class FoxApi {
       $clearval[]='"'.addslashes($v).'"';
 
     $sql='INSERT INTO '.$table.' ('.join(',',$fields).') values ('.join(',',$clearval).')';
-    echo $sql;
+//    echo $sql;
     $this->db->Execute($sql);
   }
+
   function sql($sql){
     return $this->db->Execute($sql);
   }
+  function queryData($url=""){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$this->urlBase.$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADERFUNCTION, "HandleHeaderLine");
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $header = substr($response, 0, $header_size);
+    $body = substr($response, $header_size);
+    $this->applyItemJSON($body);
+  }
+
+
 }
 ?>
--- server.php
+++ server.php
@@ -12,64 +12,15 @@ try {
   }
 
   require "foxapi.php";
-  $conn = new COM("ADODB.Connection") or die("Cannot start ADO");
-
-  $conn->Open("Provider = VFPOLEDB.1; Data Source = \"C:\\Users\\Dev\\Desktop\\FoxProProj\\db_test\\a0a.dbf\"");
-  $api=new FoxApi($conn);
+  $api=new FoxApi(true);
 
   $mode=empty($_REQUEST['mode'])?'':$_REQUEST['mode'];
   switch($mode) {
-    case 'insert':
-      //
-      $api->insert("b131",array(
-         "nsert"=>"2018Данные для ДПС.23.01.00003",
-         "id_13"=>1,
-         "p2_a1_1"=>"это тест"
-       ));
-    break;
     case "request":
-      // $f=fopen($_REQUEST['url'],"r");
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,"http://192.168.3.54:5002/kz");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_HEADERFUNCTION, "HandleHeaderLine");
-      $response = curl_exec($ch);
-      curl_close($ch);
-      $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-      $header = substr($response, 0, $header_size);
-      $body = substr($response, $header_size);
-
-
-      echo $header,'<hr>',$body;
-//      $err[]=$content;
-      //$err[]=$_REQUEST['url'];
-      //$err[]=join('',file($_REQUEST['url']));
+      $testJSON=join("",file("responce.js"));
+      $api->saveData($testJSON);
     break;
-    case 'sql':
-      $sql=$_REQUEST['data'];
-      if($sql){
-        $result=$api->sql($sql);
-
-        if ($result === false) die("failed");
-        print_r($result);
-
-  // Section 3
-        while (!$result->EOF) {
-          
-          echo $result->fields[0]," ",$result->fields[1]," ",$result->fields[2];
-          // for ($i=0, $max=$result->FieldCount(); $i < $max; $i++) {
-          //   print $result->fields[$i].' ';
-          // }
-          $result->MoveNext();
-          print "<br>\n";
-        }
-
-      }
-
-    break;
-};
-$conn->Close();
-
+  };
 } catch (Exception $e){
   $err[]=$e->getMessage();
 }
@@ -81,6 +32,7 @@ if(count($err)){
 }
 
 
+
 header('Content-type:text/html,codepage=utf-8');
 echo <<<EOT
 <!DOCTYPE html>
@@ -90,27 +42,13 @@ echo <<<EOT
 <title></title>
 </head>
 <body>
-$viewerr
-<h2>Test insert</h2>
-<form action="server.php">
-<input type="hidden" name="mode" value="insert">
-<button>Submit</button>
-</form>
-<hr>
-
-<h2>Test sql</h2>
-<form action="server.php">
-<input type="hidden" name="mode" value="sql">
-<textarea name="data" cols=5 style="width:100%">$sql</textarea>
-<button>Submit</button>
-</form>
-<hr>
 <h2>Request</h2>
 <form action="server.php">
 <input type="hidden" name="mode" value="request">
-<input type="text" style="width:100%" name="url" value="http://192.168.3.54:5002/kz?id=2">
+{$api->apiURL}&nbsp;<input type="text" style="width:200px" name="url" value="?id=2">
 <button>Do request</button>
 <hr>
+$viewerr
 </form>
 </body>
 </html>
